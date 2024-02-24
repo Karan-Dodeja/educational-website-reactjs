495e2433909197f59784aa949e56f82c50239e6c
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-02-24 19:55:12 +0530

    Team section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Router } from "react-router-dom";
 import Home from "./components/Home/Home";
 import About from "./components/about/About";
 import CourseHome from "./components/allcourses/CourseHome";
+import Team from "./components/Home/team/Team.jsx";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" exact Component={<Home />}></Route>
         <Route path="/about" Component={<About />}></Route>
         <Route path="/courses" Component={<CourseHome />}></Route>
+        <Route path="/team" Component={<Team />}></Route>
       </Router>
     </div>
   );
