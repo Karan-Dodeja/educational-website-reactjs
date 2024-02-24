03616bf6ddabeca881585a51653b5e379272cbea
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-02-24 11:45:31 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import Header from "./components/common/header/Header";
+import { Route, Router } from "react-router-dom";
 
-function App() {
-  return <div className="App">App.js file</div>;
-}
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Header />
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/common/header/Header.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Head from "./Head";
+import "./head.css";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const [click, setClick] = useState(false);
+  return (
+    <>
+      <Head />
+      <header>
+        <nav className="flexSB">
+          <ul
+            className={click ? "mobile-nav" : "flexSB"}
+            onClick={() => setClick(false)}
+          >
+            <li>
+              <Link to="/">Home</Link>
+              <Link to="/courses">All Courses</Link>
+              <Link to="/about">About</Link>
+              <Link to="/team">Team</Link>
+              <Link to="/pricing">Pricing</Link>
+              <Link to="/journal">Journal</Link>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+          <div className="start">
+            <div className="button">GET CERTIFICATE</div>
+          </div>
+          <button className="toggle" onClick={() => setClick(!click)}>
+            {click ? (
+              <i className="fa fa-times"></i>
+            ) : (
+              <i className="fa fa-bars"></i>
+            )}
+          </button>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
