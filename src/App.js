f19428a3ab3b398d392c2dbaad64f85a6a0f5213
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-02-24 16:40:14 +0530

    All courses section

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import Header from "./components/common/header/Header";
 import { Route, Router } from "react-router-dom";
 import Home from "./components/Home/Home";
 import About from "./components/about/About";
+import CourseHome from "./components/allcourses/CourseHome";
 
 const App = () => {
   return (
     <div>
       <Router>
         <Header />
-        <Route path="/about" Component={<About />}></Route>
-        <Route path="/users">{/* <Users /> */}</Route>
         <Route path="/" exact Component={<Home />}></Route>
+        <Route path="/about" Component={<About />}></Route>
+        <Route path="/courses" Component={<CourseHome />}></Route>
       </Router>
     </div>
   );
